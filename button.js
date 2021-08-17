63238b169f153082cac2050bd1c8529daf2272cc
FarhadIslamMim <76618171+FarhadIslamMim@users.noreply.github.com>
2021-08-17 22:00:20 +0600

    Add files via upload

---
+++ button.js
@@ -0,0 +1,22 @@
+var newp=document.querySelector("#p");
+var image=document.querySelector("#imgID");
+function button1(){
+   
+    newp.innerHTML="you have clicked button1<br>thank you!";
+}
+
+function button2(){
+    
+    newp.innerHTML="you have clicked <b>button2</b><br>thank you!";
+}
+
+
+function mim(){
+    
+    image.src="images/mim.jpg";
+}
+
+function ship(){
+    
+    image.src="images/ship.jpg";
+}
